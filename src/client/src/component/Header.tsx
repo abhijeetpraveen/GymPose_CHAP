186b13bf469d3b39b72f2200e0e26b0d65a6a5b9
Abhigyan Praveen <abhigyan.praveen@mail.mcgill.ca>
2022-10-02 01:46:45 -0400

    colourchanges

--- src/client/src/component/Header.tsx
+++ src/client/src/component/Header.tsx
@@ -83,7 +83,7 @@ export function HeaderMenuColored({ links }: HeaderSearchProps) {
       <Container>
         <div className={classes.inner}>
           <div style = {{display:"flex"}}>
-          <Image src="https://i.imgur.com/TI14jcU.png" 
+          <Image src="https://i.imgur.com/7sAonl1.png"
             width={70} height={50}/>
           <div className = {classes.headertitle}> GYMPOSE </div>
           </div>
